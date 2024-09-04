2b0064b7b1d8f2a67c69748736d0d92bbc671299
Angel <angel@example.com>
2024-09-04 15:15:04 +0300

    76

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -94,6 +94,5 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
