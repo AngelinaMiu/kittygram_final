3c0bb51d7cb2301c0b93b5aacbd207c96642a77a
Angelina Makarova <AngelinaMiu11@yandex.ru>
2024-09-11 19:55:24 +0000

    2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -93,24 +93,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs/django.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'backend/staticfiles'
