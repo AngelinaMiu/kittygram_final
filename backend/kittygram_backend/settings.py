893741a9ffa9edc760f3cf0f084c07f7d1c75ee2
Angelina Makarova <AngelinaMiu11@yandex.ru>
2024-10-16 21:21:38 +0000

    4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql'
-        'NAME': os.getenv('DB_NAME', 'kittygram'),
+        'NAME': str(os.getenv('DB_NAME', 'kittygram')),
         'USER': os.getenv('DB_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'yourpassword'),
         'HOST': os.getenv('DB_HOST', 'localhost,127.0.0.1,backend,finalkittygramiu.tech,51.250.105.252'),
